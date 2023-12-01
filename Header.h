616e46ad0935b8e22fe8892d5480481e17fc265e
5trawb3rry <127856208+5trawb3rry@users.noreply.github.com>
2023-12-01 21:15:11 +0500

    Add files via upload
    UPDATED HEADER 
    FIXED THE SCREEN GLITCHING

--- Header.h
+++ Header.h
@@ -1,186 +1,165 @@
 ﻿#pragma once
 #include <iostream>
 #include <windows.h>
-#include <Windows.h>
 #include <mmsystem.h>
 #include <conio.h>
 
-//global variable for option selecton (other functions were not working, had to resort to this)
 int prime = 0;
 using namespace std;
-
 void menu();
 int navigateMenu(int currentOption, int totalOptions);
 
-
-//FUNCTION TO START THE GAME
 void gameLogic()
 {
-	cout << "STARTED";
+    cout << "STARTED";
 }
 
 void displayTestMessage()
 {
-	cout << "test" << endl;
+    // Implement the logic to display a test message
+    cout << "Starting the game! (Test Message)\n";
+    // You can replace this with the actual logic for starting the game
 }
 
-
-//BEEP SOUND EFFECT
 void startSound()
 {
-	Beep(500, 500);
-	Sleep(500);
-	Beep(500, 550);
-	Sleep(500);
-	Beep(501, 950);
+    Beep(500, 500);
+    Sleep(500);
+    Beep(500, 550);
+    Sleep(500);
+    Beep(501, 950);
 }
 
 void setColor(int color)
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
 void resetColor()
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // Reset to default color
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // Reset to default color
 }
 
-
-//MENU FOR ARROW KEY SELECTION
 void menuOption(int option)
 {
-	system("cls");
-
-	if (option == 1)
-	{
-		menu();
-		cout << ">START GAME" << endl;
-		cout << " RESUME GAME" << endl;
-		cout << " TEST BUTTON" << endl;
-		cout << " TEST BUTTON" << endl;
-		prime = 1;
-	}
-
-	if (option == 2)
-	{
-		menu();
-		cout << " START GAME" << endl;
-		cout << ">RESUME GAME" << endl;
-		cout << " TEST BUTTON" << endl;
-		cout << " TEST BUTTON" << endl;
-		prime = 2;
-	}
-
-	if (option == 3)
-	{
-		menu();
-		cout << " START GAME" << endl;
-		cout << " RESUME GAME" << endl;
-		cout << ">TEST BUTTON" << endl;
-		cout << " TEST BUTTON" << endl;
-		prime = 3;
-	}
-	
-	if (option == 4)
-	{
-		menu();
-		cout << " START GAME" << endl;
-		cout << " RESUME GAME" << endl;
-		cout << " TEST BUTTON" << endl;
-		cout << ">TEST BUTTON" << endl;
-	}
-
-	char key = _getch();
+    prime = option;
+    menu();
+    switch (option)
+    {
+    case 1:
+        cout << "\
+>START GAME\n\
+ RESUME GAME\n\
+ TEST BUTTON\n\
+ TEST BUTTON\n";
+        prime = 1;
+        break;
+    case 2:
+        cout << "\
+ START GAME\n\
+>RESUME GAME\n\
+ TEST BUTTON\n\
+ TEST BUTTON\n";
+        prime = 2;
+        break;
+    case 3:
+        cout << "\
+ START GAME\n\
+ RESUME GAME\n\
+>TEST BUTTON\n\
+ TEST BUTTON\n";
+        prime = 3;
+        break;
+    case 4:
+        cout << "\
+ START GAME\n\
+ RESUME GAME\n\
+ TEST BUTTON\n\
+>TEST BUTTON\n";
+        prime = 4;
+        break;
+    }
 
 
-	// FOR SELECTION OF OPTIONS
-	if (key == 13 && prime == 1) 
-	{
-		startSound();
-		system("cls");
-		gameLogic();
-	}
+    char key = _getch();
 
 
-	if (key == 13 && prime == 2)
-	{
-		system("cls");
-		displayTestMessage();
-	}
+    // FOR SELECTION OF OPTIONS
+    if (key == 13 && prime == 1)
+    {
+        startSound();
+        system("cls");
+        gameLogic();
+    }
 
 
-	if (key == 13 && prime == 3)
-	{
-		displayTestMessage();
-	}
+    if (key == 13 && prime == 2)
+    {
+        system("cls");
+        displayTestMessage();
+    }
 
 
-	if (key == 13 && prime == 4)
-	{
-		displayTestMessage();
-	}
+    if (key == 13 && prime == 3)
+    {
+        displayTestMessage();
+    }
 
+
+    if (key == 13 && prime == 4)
+    {
+        displayTestMessage();
+    }
 }
-	
 
 void menu()
 {
-	int choice = 0;
-	
-	setColor(11); // Set text color to light cyan
-	
-	
-
-	cout << "  ____             ____             _                                 " << endl;
-	cout << "/ ____| __ _ _ __ |  _ \\ __ _  ___(_)_ __   __ _                    " << endl;
-	cout << "| |   / _` | '__| | |_) / _` |/ __| | '_ \\ / _` |     ______        " << endl;
-	cout << "| |__| (_| | |    |  _ < (_| | (__| | | | | (_| |    /|_||_\\` __    " << endl;
-	cout << " \\____\\__,_|_|    |_| \\_\\__,_|\\___|_|_| |_|\\__, |   (   _    _ _||  " << endl;
-	cout << "                                           |___/    =`-(_)--(_)-'   " << endl;
-	cout << endl;
-	cout << endl;
-	cout << "==============================================================================" << endl;
-	cout << endl;
-
-
+    system("cls");
+    int choice = 0;
+    cout << "\
+  ____             ____             _                                 \n\
+/ ____| __ _ _ __ |  _ \\ __ _  ___(_)_ __   __ _                    \n\
+| |   / _` | '__| | |_) / _` |/ __| | '_ \\ / _` |     ______        \n\
+| |__| (_| | |    |  _ < (_| | (__| | | | | (_| |    /|_||_\\` __    \n\
+ \\____\\__,_|_|    |_| \\_\\__,_|\\___|_|_| |_|\\__, |   (   _    _ _||  \n\
+                                           |___/    =`-(_)--(_)-'   \n\n\n\
+==============================================================================\n\n";
 }
 
-
-//MAIN MENU
 void mainMenu()
 {
-	int option = 1;
-	const int totalOptions = 4;
+    int option = 1;
+    const int totalOptions = 4;
+    char key = '0';
+    while (option != 99)
+    {
+        menuOption(option);
+        // Get user input and update the selected option
+        option = navigateMenu(option, totalOptions);
 
-	while (option != 99) {
-		menu();
-		menuOption(option);
-
-		// Get user input and update the selected option
-		option = navigateMenu(option, totalOptions);
-
-	}
+        // Add a small delay for better visualization (optional)
+        // Sleep(10);
+    }
 }
 
-
-
-int navigateMenu(int currentOption, int totalOptions) 
+int navigateMenu(int currentOption, int totalOptions)
 {
-	char key = _getch(); // Get a key press without displaying it on the console
-
-	// Handle arrow key input
-	switch (key) 
-	{
-	case 72: // Up arrow key
-		return (currentOption > 1) ? currentOption - 1 : totalOptions;
-
-	case 80: // Down arrow key
-		return (currentOption < totalOptions) ? currentOption + 1 : 1;
-
-	case 13:  //enter key
-		return -1;
-		 // Signal to exit the loop
-	default:
-		return currentOption; // No change
-	}
+    char key = _getch(); // skip the [
+    char arrow = _getch();
+    switch (arrow)
+    { // the real value
+    case 'A':
+    case 'H':
+        // code for arrow up
+        return (currentOption > 1) ? currentOption - 1 : totalOptions;
+        break;
+    case 'B':
+    case 'P':
+        return (currentOption < totalOptions) ? currentOption + 1 : 1;
+        // code for arrow down
+        break;
+    default:
+        cout << key;
+        return currentOption; // No change
+    }
 }
